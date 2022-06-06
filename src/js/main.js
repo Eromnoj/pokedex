993e0333da07462802cdf6ec6e7d67317e8b613c
eromnoj <j.moreschi@outlook.fr>
2022-06-06 15:33:08 +0200

    Add infinite scroll

--- src/js/main.js
+++ src/js/main.js
@@ -1,12 +1,21 @@
 const pokedex = document.querySelector('.pokedex')
+const prevButton = document.querySelector('.previous')
+const nextButton = document.querySelector('.next')
 
+let apiAddress = new URL('https://pokeapi.co/api/v2/pokemon/?offset=0&limit=16')
+
+let limit
+let prevAddress
+let nextAddress
+let trigger = window.innerHeight
+console.log(trigger)
 const getPokemon = (name, img, color) => {
   let divPoke = document.createElement('div')
   divPoke.className = `pokemon ${color}`
   divPoke.id = name
   divPoke.style.backgroundColor = color
   pokedex.appendChild(divPoke)
-
+  divPoke.style.opacity = 1
   let namePoke = document.createElement('p')
   namePoke.className = 'name-poke'
   namePoke.innerText = name
@@ -18,31 +27,50 @@ const getPokemon = (name, img, color) => {
   divPoke.appendChild(imgPoke)
 }
 
-fetch('https://pokeapi.co/api/v2/pokemon/?offset=0&limit=20')
-.then(response => response.json())
-.then(data => {
+const fetchPokemon = (address) => {
+  // pokedex.innerHTML = ""
+  trigger = pokedex.scrollHeight
+  fetch(address)
+    .then(response => response.json())
+    .then(data => {
 
-  Array.from(data.results).forEach(url => {
-    fetch(url.url)
-  .then(response => response.json())
-  .then(data => {
+      console.log(address)
+      setTimeout(() => {
+        Array.from(data.results).forEach(url => {
 
-    console.log(data.species.url)
-    fetch(data.species.url)
-      .then(response => response.json())
-      .then(data2 => {
-        getPokemon(data2.names[4].name, data.sprites.other["official-artwork"].front_default, data2.color.name)
-      })
-      .catch(err => {
-        console.log(err)
-      })
-  })
-  .catch(err => {
-    console.log(err)
-  })
+          nextAddress = data.next
+          fetch(url.url)
+            .then(response => response.json())
+            .then(data => {
 
-  })
+              fetch(data.species.url)
+                .then(response => response.json())
+                .then(data2 => {
+
+                  getPokemon(data2.names[4].name, data.sprites.other["official-artwork"].front_default, data2.color.name)
+                })
+                .catch(err => {
+                  console.log(err)
+                })
+            })
+            .catch(err => {
+              console.log(err)
+            })
+
+        })
+      }, 500)
+
+    })
+}
+
+fetchPokemon(apiAddress)
+
+window.addEventListener('scroll', () => {
+
+  limit = window.scrollY
+  console.log(limit)
+  if (limit >= trigger) {
+    fetchPokemon(nextAddress)
+    console.log(limit, trigger)
+  }
 })
-
-
-
